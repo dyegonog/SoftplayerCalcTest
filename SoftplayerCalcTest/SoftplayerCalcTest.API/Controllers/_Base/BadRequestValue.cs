846d443adb46314d9e43de55f35ef39c6f7b5db2
dyegonog <dyegoec@outlook.com>
2019-04-11 14:06:58 -0400

    adicionando base controller

---
+++ SoftplayerCalcTest/SoftplayerCalcTest.API/Controllers/_Base/BadRequestValue.cs
@@ -0,0 +1,24 @@
+﻿using SoftplayerCalcTest.Aplicacao._Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftplayerCalcTest.API.Controllers._Base
+{
+    public class BadRequestValue
+    {
+        public string Mensagem { get; }
+        public IReadOnlyCollection<Notification> Erros { get; }
+
+        public BadRequestValue(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public BadRequestValue(string mensagem, IReadOnlyCollection<Notification> erros) : this(mensagem)
+        {
+            Erros = erros;
+        }
+    }
+}
---
+++ SoftplayerCalcTest/SoftplayerCalcTest.API/Controllers/_Base/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftplayerCalcTest.Aplicacao._Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftplayerCalcTest.API.Controllers._Base
+{
+    public class BaseController : Controller
+    {
+        public IActionResult OkResponse<TCommand, TResponse>(TCommand command, CommandHandler<TCommand, TResponse> commandHandler)
+            where TCommand : Command
+            where TResponse : HandlerResponse
+        {
+            if (!command.IsValid()) return CreateBadRequestForInvalidInput(command);
+
+            var response = commandHandler.Executar(command);
+
+            return Ok(response);
+        }
+
+        private BadRequestObjectResult CreateBadRequestForInvalidInput(Command command)
+        {
+            return BadRequest(new BadRequestValue("Algumas informações estão com valores inválidos", command.Notifications));
+        }
+    }
+}
