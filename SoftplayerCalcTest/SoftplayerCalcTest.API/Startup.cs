d14016a7b84c2167f39c5167ca038245cdc1ce33
dyegonog <dyegoec@outlook.com>
2019-04-12 17:32:08 -0400

    corrigindo erro do swagger 404

--- SoftplayerCalcTest/SoftplayerCalcTest.API/Startup.cs
+++ SoftplayerCalcTest/SoftplayerCalcTest.API/Startup.cs
@@ -54,7 +54,7 @@ namespace SoftplayerCalcTest.API
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                c.RoutePrefix = "swagger";
             });
 
             app.UseHttpsRedirection();
