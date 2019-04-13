5e2207ff6ad0595fcaf62a9dbcdd6c3ea9ca3297
dyegonog <dyegoec@outlook.com>
2019-04-13 12:20:45 -0400

    adicionando teste de unidade para o dominio

---
+++ SoftplayerCalcTest/SoftplayerCalcTest.TestesDeUnidade/Dominio/JurosDomain/JurosBuilder.cs
@@ -0,0 +1,32 @@
+﻿using SoftplayerCalcTest.Dominio;
+
+namespace SoftplayerCalcTest.TestesDeUnidade.Dominio
+{
+    public class JurosBuilder
+    {
+        private int _tempo;
+        private decimal _valorInicial;
+
+        public static JurosBuilder Novo()
+        {
+            return new JurosBuilder();
+        }
+
+        public JurosBuilder ComTempo(int tempo)
+        {
+            _tempo = tempo;
+            return this;
+        }
+
+        public JurosBuilder ComValorInicial(decimal valorInicial)
+        {
+            _valorInicial = valorInicial;
+            return this;
+        }
+
+        public JurosDomain Build()
+        {
+            return new JurosDomain(_tempo, _valorInicial);
+        }
+    }
+}
---
+++ SoftplayerCalcTest/SoftplayerCalcTest.TestesDeUnidade/Dominio/JurosDomain/JurosTeste.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SoftplayerCalcTest.TestesDeUnidade.Dominio.Juros.Domain
+{
+    public class JurosTeste
+    {
+        [Theory]
+        [InlineData(100, 5)]
+        public void Deve_Calcula_Juros(decimal valorEsperado, int tempo)
+        {
+            var juros = JurosBuilder.Novo().ComValorInicial(valorEsperado).ComTempo(tempo).Build();
+
+            var esperado = (decimal)105.10;
+            Assert.Equal(esperado, juros.Calcular());
+        }
+    }
+}
