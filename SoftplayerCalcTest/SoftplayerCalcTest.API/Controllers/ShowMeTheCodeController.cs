b561b24aa5581d4557133819371fa92573a4c656
dyegonog <dyegoec@outlook.com>
2019-04-13 08:50:50 -0400

    adicionado controller showmethecode

---
+++ SoftplayerCalcTest/SoftplayerCalcTest.API/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftplayerCalcTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : Controller
+    {
+        [HttpGet]
+        public string ObterCodigoFonte()
+        {
+            return "https://github.com/dyegonog/SoftplayerCalcTest";
+        }
+    }
+}
